bbe3cf18fda03567faed3688d9f13817b769107a
ShenanVindinu <shenanvindinu@gmail.com>
2024-10-13 19:57:41 +0530

    CustomerService interface created

---
+++ src/main/java/lk/ijse/gdse/aad67/posbackendspring/service/CustomerService.java
@@ -0,0 +1,8 @@
+package lk.ijse.gdse.aad67.posbackendspring.service;
+
+import lk.ijse.gdse.aad67.posbackendspring.dto.CustomerDTO;
+
+public interface CustomerService {
+
+    void saveCustomer(CustomerDTO customerDTO);
+}
