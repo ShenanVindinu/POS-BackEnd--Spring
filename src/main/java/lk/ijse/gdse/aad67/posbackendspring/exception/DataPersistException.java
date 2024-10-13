0e7027eab1ff00a9d1ea820186b7bfaa03f111be
ShenanVindinu <shenanvindinu@gmail.com>
2024-10-13 19:56:12 +0530

    Data Persist exception created

---
+++ src/main/java/lk/ijse/gdse/aad67/posbackendspring/exception/DataPersistException.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.aad67.posbackendspring.exception;
+
+public class DataPersistException extends RuntimeException {
+    public DataPersistException() {
+    }
+
+    public DataPersistException(String message) {
+
+    }
+    public DataPersistException(String message, Throwable cause) {
+
+    }
+}
