687241836d88d73654a380e162f4382e72565794
ShenanVindinu <shenanvindinu@gmail.com>
2024-10-13 19:55:58 +0530

    Customer Controller created

--- src/main/java/lk/ijse/gdse/aad67/posbackendspring/controller/CustomerController.java
+++ src/main/java/lk/ijse/gdse/aad67/posbackendspring/controller/CustomerController.java
@@ -1,4 +1,50 @@
 package lk.ijse.gdse.aad67.posbackendspring.controller;
 
+import lk.ijse.gdse.aad67.posbackendspring.dto.CustomerDTO;
+import lk.ijse.gdse.aad67.posbackendspring.exception.DataPersistException;
+import lk.ijse.gdse.aad67.posbackendspring.service.impl.CustomerServiceIMPL;
+import lk.ijse.gdse.aad67.posbackendspring.util.AppUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/customer")
 public class CustomerController {
+
+    @Autowired
+    private CustomerServiceIMPL customerServiceIMPL;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveCustomer(
+            @RequestPart("name") String name,
+            @RequestPart("address") String address,
+            @RequestPart("salary") String salary
+    ) {
+        try {
+            //Id generation
+            String cusId = AppUtil.generateCustomerId();
+
+            //Build Obj
+            CustomerDTO buildCustomerDTO = new CustomerDTO();
+            buildCustomerDTO.setName(name);
+            buildCustomerDTO.setAddress(address);
+            buildCustomerDTO.setSalary(salary);
+            customerServiceIMPL.saveCustomer(buildCustomerDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+
+        } catch (DataPersistException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
