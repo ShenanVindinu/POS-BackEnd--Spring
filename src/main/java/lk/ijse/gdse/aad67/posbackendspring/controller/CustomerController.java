904ec01e22789485da5650d7b9df3df2f992c1f5
ShenanVindinu <shenanvindinu@gmail.com>
2024-10-14 18:05:04 +0530

    Used RequestBody Annotation instead of previous. PostMapping Corrected

--- src/main/java/lk/ijse/gdse/aad67/posbackendspring/controller/CustomerController.java
+++ src/main/java/lk/ijse/gdse/aad67/posbackendspring/controller/CustomerController.java
@@ -3,15 +3,11 @@ package lk.ijse.gdse.aad67.posbackendspring.controller;
 import lk.ijse.gdse.aad67.posbackendspring.dto.CustomerDTO;
 import lk.ijse.gdse.aad67.posbackendspring.exception.DataPersistException;
 import lk.ijse.gdse.aad67.posbackendspring.service.CustomerService;
-import lk.ijse.gdse.aad67.posbackendspring.util.AppUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/customer")
@@ -21,21 +17,9 @@ public class CustomerController {
     private CustomerService customerService;
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> saveCustomer(
-            @RequestPart("name") String name,
-            @RequestPart("address") String address,
-            @RequestPart("salary") String salary
-    ) {
+    public ResponseEntity<Void> saveCustomer(@RequestBody CustomerDTO customerDTO) {
         try {
-            //Id generation
-            String cusId = AppUtil.generateCustomerId();
-
-            //Build Obj
-            CustomerDTO buildCustomerDTO = new CustomerDTO();
-            buildCustomerDTO.setName(name);
-            buildCustomerDTO.setAddress(address);
-            buildCustomerDTO.setSalary(salary);
-            customerService.saveCustomer(buildCustomerDTO);
+            customerService.saveCustomer(customerDTO);
             return new ResponseEntity<>(HttpStatus.CREATED);
 
         } catch (DataPersistException e) {
--- src/main/java/lk/ijse/gdse/aad67/posbackendspring/service/impl/CustomerServiceIMPL.java
+++ src/main/java/lk/ijse/gdse/aad67/posbackendspring/service/impl/CustomerServiceIMPL.java
@@ -6,6 +6,7 @@ import lk.ijse.gdse.aad67.posbackendspring.dto.CustomerDTO;
 import lk.ijse.gdse.aad67.posbackendspring.entity.impl.CustomerEntity;
 import lk.ijse.gdse.aad67.posbackendspring.exception.DataPersistException;
 import lk.ijse.gdse.aad67.posbackendspring.service.CustomerService;
+import lk.ijse.gdse.aad67.posbackendspring.util.AppUtil;
 import lk.ijse.gdse.aad67.posbackendspring.util.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,7 @@ public class CustomerServiceIMPL implements CustomerService {
 
     @Override
     public void saveCustomer(CustomerDTO customerDTO) {
+        customerDTO.setId(AppUtil.generateCustomerId());
         CustomerEntity savedUser =
                 customerDAO.save(mapping.toCustomerEntity(customerDTO));
         if (savedUser == null) {
