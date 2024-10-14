3007de0ceec0b2e66187e1eb64c8c9503256d91a
ShenanVindinu <shenanvindinu@gmail.com>
2024-10-14 22:02:09 +0530

    asCustomerDTOList mapping added

--- src/main/java/lk/ijse/gdse/aad67/posbackendspring/util/Mapping.java
+++ src/main/java/lk/ijse/gdse/aad67/posbackendspring/util/Mapping.java
@@ -3,8 +3,11 @@ package lk.ijse.gdse.aad67.posbackendspring.util;
 import lk.ijse.gdse.aad67.posbackendspring.dto.CustomerDTO;
 import lk.ijse.gdse.aad67.posbackendspring.entity.impl.CustomerEntity;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class Mapping {
@@ -15,5 +18,9 @@ public class Mapping {
 
     public CustomerEntity toCustomerEntity(CustomerDTO customerDTO) {
         return modelMapper.map(customerDTO, CustomerEntity.class);
+    }
+
+    public List<CustomerDTO> asCustomerDTOList(List<CustomerEntity> all) {
+        return modelMapper.map(all, new TypeToken<List<CustomerDTO>>() {}.getType());
     }
 }
