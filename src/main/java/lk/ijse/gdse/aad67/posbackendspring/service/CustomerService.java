2c751af495e774d2667f1a19cc4b2f4ef835f3bc
ShenanVindinu <shenanvindinu@gmail.com>
2024-10-14 22:00:50 +0530

    getall customers loose coupled using interface

--- src/main/java/lk/ijse/gdse/aad67/posbackendspring/service/CustomerService.java
+++ src/main/java/lk/ijse/gdse/aad67/posbackendspring/service/CustomerService.java
@@ -2,7 +2,11 @@ package lk.ijse.gdse.aad67.posbackendspring.service;
 
 import lk.ijse.gdse.aad67.posbackendspring.dto.CustomerDTO;
 
+import java.util.List;
+
 public interface CustomerService {
 
     void saveCustomer(CustomerDTO customerDTO);
+
+    List<CustomerDTO> getAllCustomers();
 }
