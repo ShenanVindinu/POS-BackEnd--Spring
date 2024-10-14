b708a4f811fd89e5eca367a47812d060a58ac1af
ShenanVindinu <shenanvindinu@gmail.com>
2024-10-15 01:04:42 +0530

    ItemDAO created

---
+++ src/main/java/lk/ijse/gdse/aad67/posbackendspring/dao/ItemDAO.java
@@ -0,0 +1,8 @@
+package lk.ijse.gdse.aad67.posbackendspring.dao;
+
+import lk.ijse.gdse.aad67.posbackendspring.entity.impl.ItemEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemDAO extends JpaRepository<ItemEntity,String> {
+
+}
