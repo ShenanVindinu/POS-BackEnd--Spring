6a08116b04688305ee33933c10d22a1796880344
ShenanVindinu <shenanvindinu@gmail.com>
2024-10-15 01:05:32 +0530

    SaveItem and GetAll Item method signatures added

---
+++ src/main/java/lk/ijse/gdse/aad67/posbackendspring/service/ItemService.java
@@ -0,0 +1,12 @@
+package lk.ijse.gdse.aad67.posbackendspring.service;
+
+import lk.ijse.gdse.aad67.posbackendspring.dto.ItemDTO;
+
+import java.util.List;
+
+public interface ItemService {
+
+    void saveItems(ItemDTO item);
+
+    List<ItemDTO> getAllItems();
+}
