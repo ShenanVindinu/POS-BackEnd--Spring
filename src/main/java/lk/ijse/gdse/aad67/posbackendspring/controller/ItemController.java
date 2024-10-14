b7214b59d79c955de8f84d9d2b4c3ac78fb4ded0
ShenanVindinu <shenanvindinu@gmail.com>
2024-10-15 01:05:11 +0530

    SaveItem and GetAll Item Created

---
+++ src/main/java/lk/ijse/gdse/aad67/posbackendspring/controller/ItemController.java
@@ -0,0 +1,38 @@
+package lk.ijse.gdse.aad67.posbackendspring.controller;
+
+import lk.ijse.gdse.aad67.posbackendspring.dto.ItemDTO;
+import lk.ijse.gdse.aad67.posbackendspring.exception.DataPersistException;
+import lk.ijse.gdse.aad67.posbackendspring.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/item")
+public class ItemController {
+    @Autowired
+    private ItemService itemService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveItem(@RequestBody ItemDTO item) {
+        try {
+            itemService.saveItems(item);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (DataPersistException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<ItemDTO> getAllItems() {
+        return itemService.getAllItems();
+    }
+}
