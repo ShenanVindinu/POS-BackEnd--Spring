fa069236f0a35f1c0cde7f8e5f88df3080e37c02
ShenanVindinu <shenanvindinu@gmail.com>
2024-10-14 23:21:22 +0530

    delete and update method signatures added

--- src/main/java/lk/ijse/gdse/aad67/posbackendspring/service/CustomerService.java
+++ src/main/java/lk/ijse/gdse/aad67/posbackendspring/service/CustomerService.java
@@ -1,6 +1,7 @@
 package lk.ijse.gdse.aad67.posbackendspring.service;
 
 import lk.ijse.gdse.aad67.posbackendspring.dto.CustomerDTO;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
@@ -9,4 +10,8 @@ public interface CustomerService {
     void saveCustomer(CustomerDTO customerDTO);
 
     List<CustomerDTO> getAllCustomers();
+
+    void deleteCustomerById(String cusId);
+
+    void updateCustomerById(String cusId, CustomerDTO updatedCustomerDTO);
 }
